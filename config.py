c4ab4653d3e4a1912d20a367f2b25e315d537bd4
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-12-11 17:16:38 +0200

    Adds manage and config modules for running development server #9

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DEBUG = True
+
+
+MONGODB_SETTINGS = {
+'DB': "project_runner"
+}
+
+SECRET_KEY = "123?"
---
+++ manage.py
@@ -0,0 +1,48 @@
+#!/bin/env python
+from flask import url_for
+from flask.ext.script import Manager, Shell
+from application import app, db, models, api
+from application.resources import user, token
+
+
+manager = Manager(app)
+
+
+def _make_context():
+    return dict(app=app, db=db, models=models)
+
+manager.add_command("shell", Shell(make_context=_make_context))
+
+
+@manager.command
+def drop():
+    """Drops database"""
+    db.connection.drop_database(app.config['MONGODB_SETTINGS']['DB'])
+
+
+@manager.command
+def run():
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+
+@manager.command
+def routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote(
+            "{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+
+    for line in sorted(output):
+        print line
+
+if __name__ == "__main__":
+    manager.run()
