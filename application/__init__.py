9fdac2e9b22d046c93fa2f4efc7a27353fd001ac
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-12-27 02:22:42 +0200

    Removes CORS headers from tree (handled by reverse proxy)

--- application/__init__.py
+++ application/__init__.py
@@ -33,12 +33,3 @@ errors = {
 
 api = Api(app, catch_all_404s=True, errors=errors)
 
-def add_cors_header(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'HEAD, GET, POST, PATCH, PUT, OPTIONS, DELETE'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token, Authorization'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-
-    return response
-
-app.after_request(add_cors_header)
