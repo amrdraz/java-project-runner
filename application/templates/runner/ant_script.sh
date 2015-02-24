120416344c5270732eeec85c0795c6453bdc76f1
Amr Draz <amrdraz@users.noreply.github.com>
2015-02-24 16:21:12 +0200

    Update ant_script.sh
    removed 
    ant -buildfile {{buildfile_name}} cleantestsrc

--- application/templates/runner/ant_script.sh
+++ application/templates/runner/ant_script.sh
@@ -1,6 +1,5 @@
 
 ant -buildfile {{buildfile_name}} compile
 {% if has_tests %}
-ant -buildfile {{buildfile_name}} cleantestsrc
 timeout -s 9 {{test_timeout}} ant -buildfile {{buildfile_name}} test
 {% endif %}
