bf4be7319eb7ffb17076651e543ac4c9e932b0fa
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2015-02-24 16:28:17 +0200

    Revert "Update ant_script.sh"
    This reverts commit 120416344c5270732eeec85c0795c6453bdc76f1.

--- application/templates/runner/ant_script.sh
+++ application/templates/runner/ant_script.sh
@@ -1,5 +1,6 @@
 
 ant -buildfile {{buildfile_name}} compile
 {% if has_tests %}
+ant -buildfile {{buildfile_name}} cleantestsrc
 timeout -s 9 {{test_timeout}} ant -buildfile {{buildfile_name}} test
 {% endif %}
