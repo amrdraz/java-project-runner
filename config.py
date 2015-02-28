beab66c452e3a23fcdd364e139f92eada9cc0178
Amr Draz <amr.m.draz@gmail.com>
2015-03-01 00:11:35 +0200

    all acounts must be activated in order to login added csv used to read files to be used as base for other batch scripts

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 DROP_ENDPOINT = True
-ENABLE_EMAIL_ACTIVATION = False
+ENABLE_EMAIL_ACTIVATION = True
 MAIL_FAIL_SILENTLY = True
 MONGODB_SETTINGS = {
     'DB': "project_runner"
