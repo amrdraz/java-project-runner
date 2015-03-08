27b5c965e6f8319e54202efc6a219dcce02e4e30
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2015-03-08 04:35:39 +0200

    Adds CSV files for first quiz

--- csv_parse.py
+++ csv_parse.py
@@ -1,32 +1,10 @@
 import csv
-import random
 from application.models import Student
 
-with open('../game_s15_teams.csv') as csvFile:
+with open('data/q1cs.csv') as csvFile:
     reader = csv.DictReader(csvFile)
     for row in reader:
-        print(row['guc_id'], row['email'])
-        email = row['email']
-        try:
-            student = Student.objects.get(email=email)
-            print("found student")
-            student.active = True
-            student.guc_id = row['guc_id']
-            student.team_id = row['team_id']
-            student.name = row['name']
-            student.tutorial = row['tutorial']
-            student.major = row['major']
-        except:
-            print("did not find student", email)
-            student = Student(
-                email=email,
-                password=random.randrange(100000000, 1000000000),
-                active=True,
-                guc_id=row['guc_id'],
-                team_id=row['team_id'],
-                name=row['name'],
-                tutorial=row['tutorial'],
-                major=row['major'])
+        student = Student.objects.get(email__iexact=row['GUC Email'])
+        student.verification_code = row['Quiz Password code']
         student.save()
-        student.reload()
-        print(student.to_dict())
+        
