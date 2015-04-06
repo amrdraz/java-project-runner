c1b7f7e29edafa53d674c43ede199c2a7fd7cc1a
Amr Draz <amr.m.draz@gmail.com>
2015-04-06 20:10:20 +0200

    added script used to generate project grade.csv

---
+++ project_grades_csv.py
@@ -0,0 +1,15 @@
+
+def project_grades_csv(project_id):
+    import csv
+    from application.models import Project
+
+    project = Project.objects.get(id=project_id)
+    with open(project.name+' Grades.csv', 'w') as csvfile:
+        fieldnames = ['team_id','guc_id','name','email','project','grade','submitter']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in project.student_results_for_csv():
+            writer.writerow(row)
+
+# example
+# project_grades_csv("54f2437d2d1f900e99057d67")
