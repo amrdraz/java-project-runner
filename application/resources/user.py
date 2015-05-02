3ed259b0e6cc7b6c672911b33a3f09a79c0e9140
Amr Draz <amr.m.draz@gmail.com>
2015-05-02 21:32:21 +0300

    split grades into 3 endpoints

--- application/resources/user.py
+++ application/resources/user.py
@@ -198,18 +198,52 @@ class UserTeamProjectGrades(Resource):
             team_grades = [
                 grade.to_dict() for grade
                 in TeamProjectGrade.objects(team_id=g.user.team_id)]
-            # return team_grades
-            quiz_grades = [
-                grade.to_dict() for grade
-                in StudentQuizGrade(student=g.user)]
+            return team_grades
+            # quiz_grades = [
+            #     grade.to_dict() for grade
+            #     in StudentQuizGrade(student=g.user)]
+            # milestone_grades = [
+            #     grade.to_dict() for grade
+            #     in StudentMilestoneGrade(student=g.user)]
+            # return {
+            #     "team_grades": team_grades,
+            #     "quiz_grades": quiz_grades,
+            #     "milestone_grades": milestone_grades,
+            # }
+        else:
+            abort(403, message="Must be a student to view grades")
+
+
+class UserMilestoneGrades(Resource):
+    method_decorators = [login_required]
+
+    @marshal_with(student_milestone_grade_fields)
+    def get(self):
+        """
+        Lists all grades related to the user.
+        """
+        if isinstance(g.user, Student):
             milestone_grades = [
                 grade.to_dict() for grade
                 in StudentMilestoneGrade(student=g.user)]
-            return {
-                "team_grades": team_grades,
-                "quiz_grades": quiz_grades,
-                "milestone_grades": milestone_grades,
-            }
+            return milestone_grades
+        else:
+            abort(403, message="Must be a student to view grades")
+
+
+class UserQuizGrades(Resource):
+    method_decorators = [login_required]
+
+    @marshal_with(student_quiz_grade_fields)
+    def get(self):
+        """
+        Lists all grades related to the user.
+        """
+        if isinstance(g.user, Student):
+            quiz_grades = [
+                grade.to_dict() for grade
+                in StudentQuizGrade(student=g.user)]
+            return quiz_grades
         else:
             abort(403, message="Must be a student to view grades")
 
@@ -218,6 +252,8 @@ api.add_resource(UsersResource, '/users', endpoint='users_ep')
 api.add_resource(UserResource, '/user/<string:id>', endpoint='user_ep')
 api.add_resource(UserSubmissions, '/user/<string:id>/submissions', endpoint='user_submissions_ep')
 api.add_resource(UserTeamProjectGrades, '/user/grades', endpoint='user_grades_ep')
+api.add_resource(UserQuizGrades, '/user/quiz_grades', endpoint='user_quiz_grades_ep')
+api.add_resource(UserMilestoneGrades, '/user/milestone_grades', endpoint='user_milestone_grades_ep')
 api.add_resource(UserActivation, '/activate', endpoint='activation_ep')
 api.add_resource(UserDashboard, '/user/dashboard', endpoint='dashboard')
 api.add_resource(UserPassReset, '/user/reset', endpoint='pass_reset_ep')
