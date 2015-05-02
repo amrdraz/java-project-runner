3ace4454636144f0e1b19b26e58dd772d6cb67fc
Amr Draz <amr.m.draz@gmail.com>
2015-05-02 09:23:02 +0300

    added the consideration of duplicate records

--- csv_parse_grades.py
+++ csv_parse_grades.py
@@ -1,14 +1,33 @@
 import csv
 from application.models import Student, StudentQuizGrade, StudentMilestoneGrade
 
+
+def saveOrUpdateQuizGrade(student, quiz, row):
+    try:
+        grade = StudentQuizGrade.objects.get(student=student, quiz=quiz)
+        print "found quiz grade"
+    except StudentQuizGrade.DoesNotExist:
+        grade = StudentQuizGrade(student=student, quiz=quiz, passed_tests=float(row["Q1_passed_tests"]), total_tests=float(row["Q1_total_tests"]), grade_in_percentage=float(row["Q1_grade_in_percentage"]))
+    grade.save()
+
+
+def saveOrUpdateMilestoneGrade(student, milestone, row):
+    try:
+        grade = StudentMilestoneGrade.objects.get(student=student, milestone=milestone)
+        print "found milestone grade"
+    except StudentMilestoneGrade.DoesNotExist:
+        grade = StudentMilestoneGrade(student=student, milestone=milestone, passed_tests=float(row["Q1_passed_tests"]), total_tests=float(row["Q1_total_tests"]), grade_in_percentage=float(row["Q1_grade_in_percentage"]))
+    grade.save()
+
 with open('../401_grades.csv') as csvFile:
     reader = csv.DictReader(csvFile)
     for row in reader:
-        student = Student.objects.get(guc_id=row['guc_id'])
-        StudentQuizGrade(student=student, quiz="Quiz 1", passed_tests=float(row["Q1_passed_tests"]), total_tests=float(row["Q1_total_tests"]), grade_in_percentage=float(row["Q1_grade_in_percentage"])).save()
-        StudentQuizGrade(student=student, quiz="Quiz 2", passed_tests=float(row["Q2_passed_tests"]), total_tests=float(row["Q2_total_tests"]), grade_in_percentage=float(row["Q2_grade_in_percentage"])).save()
-        StudentQuizGrade(student=student, quiz="Quiz 3", passed_tests=float(row["Q3_passed_tests"]), total_tests=float(row["Q3_total_tests"]), grade_in_percentage=float(row["Q3_grade_in_percentage"])).save()
+        student = Student.objects.get(guc_id=str(row['guc_id']))
 
-        StudentMilestoneGrade(student=student, milestone="Milestone 1", milestone_ratio=float(row["M1_ratio"]), grade_in_percentage=float(row['M1_final_grade_in_percentage'])).save()
-        StudentMilestoneGrade(student=student, milestone="Milestone 2", milestone_ratio=float(row["M2_ratio"]), grade_in_percentage=float(row['M2_final_grade_in_percentage'])).save()
-        StudentMilestoneGrade(student=student, milestone="Milestone 3", milestone_ratio=float(row["M3_ratio"]), grade_in_percentage=float(row['M3_final_grade_in_percentage'])).save()
+        saveOrUpdateQuizGrade(student, "Quiz 1", row)
+        saveOrUpdateQuizGrade(student, "Quiz 2", row)
+        saveOrUpdateQuizGrade(student, "Quiz 3", row)
+
+        saveOrUpdateQuizGrade(student, "Milestone 1", row)
+        saveOrUpdateQuizGrade(student, "Milestone 2", row)
+        saveOrUpdateQuizGrade(student, "Milestone 3", row)
