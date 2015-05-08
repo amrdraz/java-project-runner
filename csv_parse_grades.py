381110d64cca41dc70657e5e2115d73610e70eaf
Amr Draz <amr.m.draz@gmail.com>
2015-05-08 23:20:36 +0300

    updated csv parser to update files it finds as well as read additional grades

--- csv_parse_grades.py
+++ csv_parse_grades.py
@@ -2,34 +2,56 @@ import csv
 from application.models import Student, StudentQuizGrade, StudentMilestoneGrade
 
 
-def saveOrUpdateQuizGrade(student, quiz, pre, row):
+def saveOrUpdateQuizGrade(student, quiz, pre="", row):
+    passed_tests = float(row[pre+" - passed tests"])
+    total_tests = float(row[pre+" - total tests"])
+    grade_in_percentage = float(row[pre+" - grade %"])
     try:
         grade = StudentQuizGrade.objects.get(student=student, quiz=quiz)
         print "found "+quiz+" grade for "+student.name
     except StudentQuizGrade.DoesNotExist:
-        grade = StudentQuizGrade(student=student, quiz=quiz, passed_tests=float(row[pre+"_passed_tests"]), total_tests=float(row[pre+"_total_tests"]), grade_in_percentage=float(row[pre+"_grade_in_percentage"]))
+        print "new "+quiz+" grade for "+student.name
+        grade = StudentQuizGrade(student=student, quiz=quiz)
+
+    grade.passed_tests = passed_tests
+    grade.total_tests = total_tests
+    grade.grade_in_percentage = grade_in_percentage
     grade.save()
     return grade
 
 
-def saveOrUpdateMilestoneGrade(student, milestone, pre, row):
+def saveOrUpdateMilestoneGrade(student, milestone, pre="", row):
+    milestone_ratio = float(row[pre+" - ratio (%)"])
+    grade_in_percentage = float(row[pre+" - final grade"])
     try:
         grade = StudentMilestoneGrade.objects.get(student=student, milestone=milestone)
         print "found "+milestone+" grade for "+student.name
     except StudentMilestoneGrade.DoesNotExist:
-        grade = StudentMilestoneGrade(student=student, milestone=milestone, milestone_ratio=float(row[pre+"_ratio"]), grade_in_percentage=float(row[pre+"_final_grade_in_percentage"]))
+        print "new "+milestone+" grade for "+student.name
+        grade = StudentMilestoneGrade(student=student, milestone=milestone)
+    grade.milestone_ratio = milestone_ratio
+    grade.grade_in_percentage = grade_in_percentage
     grade.save()
     return grade
 
 with open('../401_grades.csv') as csvFile:
     reader = csv.DictReader(csvFile)
     for row in reader:
-        student = Student.objects.get(guc_id=str(row['guc_id']))
+        student = Student.objects.get(guc_id=str(row['GUC ID']))
 
-        saveOrUpdateQuizGrade(student, "Quiz 1", "Q1", row)
-        saveOrUpdateQuizGrade(student, "Quiz 2", "Q2", row)
-        saveOrUpdateQuizGrade(student, "Quiz 3", "Q3", row)
+        saveOrUpdateQuizGrade(student, "Quiz 1", "Quiz 1", row)
+        saveOrUpdateQuizGrade(student, "Quiz 2", "Quiz 2", row)
+        saveOrUpdateQuizGrade(student, "Quiz 3", "Quiz 3", row)
+        saveOrUpdateQuizGrade(student, "Quiz 4", "Quiz 4", row)
+
+        saveOrUpdateQuizGrade(student, "Project - Milestone 1 (PRM1)", "PRM1", row)
+        saveOrUpdateQuizGrade(student, "Project - Milestone 2 (PRM2)", "PRM2", row)
+        saveOrUpdateQuizGrade(student, "Project - Milestone 3 (PRM3)", "PRM3", row)
+        saveOrUpdateQuizGrade(student, "Project - Milestone 4 (PRM4)", "PRM4", row)
 
         saveOrUpdateMilestoneGrade(student, "Milestone 1", "M1", row)
         saveOrUpdateMilestoneGrade(student, "Milestone 2", "M2", row)
         saveOrUpdateMilestoneGrade(student, "Milestone 3", "M3", row)
+        saveOrUpdateMilestoneGrade(student, "Milestone 4", "M4", row)
+
+        saveOrUpdateMilestoneGrade(student, "Final Grade", "", row)
